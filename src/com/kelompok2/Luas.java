48588447f2f40a8b8cf8346cc22230528373f377
ArdaFauzan <107672285+ArdaFauzan@users.noreply.github.com>
2023-05-04 23:48:11 +0700

    Update Final

--- src/com/kelompok2/Luas.java
+++ src/com/kelompok2/Luas.java
@@ -2,23 +2,25 @@ package UTS;
 
 import java.util.Scanner;
 
-public class Luas {
+public class Luas extends Kerangka{
     public static void luasSegitiga() {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Masukkan jumlah segitiga: ");
-        int n = input.nextInt();
-        double[] luas = new double[n];
+        Scanner inputluas = new Scanner(System.in);
+
+        System.out.print("\nMasukkan Jumlah Segitiga: ");
+        int n = inputluas.nextInt();
+        Kerangka.luas = new double[n];
         for (int i = 0; i < n; i++) {
-            System.out.print("Masukkan alas segitiga ke-" + (i + 1) + ": ");
-            double alas = input.nextDouble();
-            System.out.print("Masukkan tinggi segitiga ke-" + (i + 1) + ": ");
-            double tinggi = input.nextDouble();
-            luas[i] = 0.5 * alas * tinggi;
+            System.out.print("Masukkan Alas Segitiga ke-" + (i + 1) + ": ");
+            double alas = inputluas.nextDouble();
+            System.out.print("Masukkan Tinggi Segitiga ke-" + (i + 1) + ": ");
+            double tinggi = inputluas.nextDouble();
+            System.out.println();
+            Kerangka.luas[i] = 0.5 * alas * tinggi;
         }
         System.out.println("Hasil:");
         for (int i = 0; i < n; i++) {
             System.out.println("Luas segitiga ke-" + (i + 1) + ": " + luas[i]);
         }
-        input.close();
     }
 }
+
